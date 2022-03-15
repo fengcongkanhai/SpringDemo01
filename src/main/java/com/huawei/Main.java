fc80bbb5445d8c6f87449f5018c49fb697fcc4ff
fengcong <feng.cong@aliyun.com>
2022-03-16 00:53:23 +0800

    解决maven工程打入本地包问题

--- src/main/java/com/huawei/Main.java
+++ src/main/java/com/huawei/Main.java
@@ -1,4 +1,10 @@
 package com.huawei;
 
+import cn.hutool.core.date.DateUnit;
+import cn.hutool.core.date.DateUtil;
+
 public class Main {
+    public static void main(String[] args) {
+        System.out.println(DateUtil.date());
+    }
 }
